cbc8b2a6f5e1ebd367b8fd66eec775c2f5b3553b
Bever1337 <bever1337@posteo.net>
2022-05-05 16:50:53 -0400

    feat(sync): Adds sync build step

--- prebuild.js
+++ prebuild.js
@@ -2,11 +2,21 @@ const { readFileSync, writeFileSync } = require('fs-extra')
 
 console.log('writing esm version.')
 const base = readFileSync('./src/itty-router.js', { encoding: 'utf-8' })
+writeFileSync('./dist/itty-router.mjs', base)
+const baseSync = base
+              .replace('async handle', 'handle')
+              .replace('await handler', 'handler')
+writeFileSync('./dist/itty-router-sync.js', baseSync)
 const esm = base
               .replace('function Router', 'export function Router')
               .replace('module.exports =', 'export default')
               .replace(/\s*\/\/[^\n]+/g, '')
 writeFileSync('./dist/itty-router.mjs', esm)
+const esmSync = baseSync
+              .replace('function Router', 'export function Router')
+              .replace('module.exports =', 'export default')
+              .replace(/\s*\/\/[^\n]+/g, '')
+writeFileSync('./dist/itty-router-sync.mjs', esmSync)
 
 const test = readFileSync('./src/itty-router.spec.js', { encoding: 'utf-8' })
 const minifiedTest = test.replace('itty-router', 'itty-router.min.js')
